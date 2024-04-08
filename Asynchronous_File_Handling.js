b695f9ddcea2a5bb97fe279dba98b699099f8ad2
Ajay-Das-k <ajtechhub01@gmail.com>
2024-04-08 10:14:29 +0530

    Onepoket - Node dev assessment

---
+++ Asynchronous_File_Handling.js
@@ -0,0 +1,14 @@
+import fs from "fs/promises";
+
+async function countWordsInFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    const words = data.split(/\s+/).filter((word) => word.trim() !== "");
+    const wordCount = words.length;
+    console.log("Total word count:", wordCount);
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
+}
+
+countWordsInFile("data.txt"); 
---
+++ Data_Manipulation.js
@@ -0,0 +1,4 @@
+const sumOfNumbers = (numbers) => numbers.reduce((sum, num) => sum + num, 0);
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumOfNumbers(numbers)); 
